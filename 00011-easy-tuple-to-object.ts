043ec2b37ff5e7a80a0f33b9f30eb646c5855f02
岳晓亮 <2220124666@qq.com>
2023-01-18 18:53:15 +0800

    add: 00011-easy-tuple-to-object

---
+++ 00011-easy-tuple-to-object.ts
@@ -0,0 +1,35 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils';
+
+const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const;
+const tupleNumber = [1, 2, 3, 4] as const;
+const tupleMix = [1, '2', 3, '4'] as const;
+
+type cases = [
+  Expect<Equal<TupleToObject<typeof tuple>, { tesla: 'tesla'; 'model 3': 'model 3'; 'model X': 'model X'; 'model Y': 'model Y' }>>,
+  Expect<Equal<TupleToObject<typeof tupleNumber>, { 1: 1; 2: 2; 3: 3; 4: 4 }>>,
+  Expect<Equal<TupleToObject<typeof tupleMix>, { 1: 1; '2': '2'; 3: 3; '4': '4' }>>
+];
+
+// @ts-expect-error
+type error = TupleToObject<[[1, 2], {}]>;
+
+// ============= Your Code Here =============
+
+/**
+ * const tuple = ['tesla', 'model 3', 'model X', 'model Y'] as const
+ * 转为元组字面量类型
+ */
+
+/**
+ * <T extends readonly any[]>
+ * 因为转换后的字面量类型是只读的，所以需要加上 readonly
+ */
+
+/**
+ * [K in T[number]]
+ * 遍历元组字面量的特定语法
+ */
+type TupleToObject<T extends readonly any[]> = {
+  [K in T[number]]: K;
+};
